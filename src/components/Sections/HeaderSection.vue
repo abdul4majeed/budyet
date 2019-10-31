4c2443bb89fd525ed824f7327333dd38f514aae2
AbdulMajeed <abdul4majeed@gmail.com>
2019-10-31 23:05:48 +0500

    target prop error resolved

--- src/components/Sections/HeaderSection.vue
+++ src/components/Sections/HeaderSection.vue
@@ -6,7 +6,7 @@
                 <b-navbar toggleable="sm" class="my-nav" variant="faded" type="light" fixed="top">
                     <b-navbar-brand href="#"><img src="#" alt="Logo Image"></b-navbar-brand>
 
-                    <b-navbar-toggle tmarget="nav-collapse"></b-navbar-toggle>
+                    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
                     <b-collapse id="nav-collapse" is-nav>
                         <b-navbar-nav class="m-auto main-nav">
